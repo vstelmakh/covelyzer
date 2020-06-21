228280c271f5f7d6649a92e82d2042ee732fddb4
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2020-06-22 00:31:45 +0200

    add dom document factory

---
+++ src/Xml/DocumentFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace VStelmakh\Covelyzer\Xml;
+
+class DocumentFactory
+{
+    /**
+     * Create DOMDocument. Throw exception on error.
+     *
+     * @param string $xml
+     * @param string|null $xsdFilePath
+     * @return \DOMDocument
+     */
+    public function createDocument(string $xml, ?string $xsdFilePath = null): \DOMDocument
+    {
+        $originalErrorState = \libxml_use_internal_errors(true);
+
+        $domDocument = new \DOMDocument();
+        @$domDocument->loadXML($xml, LIBXML_PARSEHUGE);
+        if ($xsdFilePath) {
+            @$domDocument->schemaValidate($xsdFilePath);
+        }
+
+        $errors = \libxml_get_errors();
+        if (!empty($errors)) {
+            $errorMessage = 'XML parse error' . PHP_EOL;
+
+            foreach ($errors as $error) {
+                $errorMessage .= sprintf(' - Line %s, column %s: %s', $error->line, $error->column, $error->message);
+            }
+
+            \libxml_clear_errors();
+            throw new \RuntimeException($errorMessage);
+        }
+
+        \libxml_use_internal_errors($originalErrorState);
+        return $domDocument;
+    }
+}
---
+++ tests/Xml/DocumentFactoryTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace VStelmakh\Covelyzer\Tests\Xml;
+
+use VStelmakh\Covelyzer\Xml\DocumentFactory;
+use PHPUnit\Framework\TestCase;
+
+class DocumentFactoryTest extends TestCase
+{
+    /**
+     * @dataProvider createDocumentDataProvider
+     *
+     * @param string $xml
+     * @param string $tag
+     * @param string|null $xsdPath
+     * @param bool $isException
+     */
+    public function testCreateDocument(string $xml, string $tag, ?string $xsdPath, bool $isException): void
+    {
+        if ($isException) {
+            $this->expectException(\RuntimeException::class);
+        }
+
+        $documentFactory = new DocumentFactory();
+        $actual = $documentFactory->createDocument($xml, $xsdPath);
+
+        $expected = new \DOMDocument();
+        $tag = $expected->createElement($tag);
+        $expected->appendChild($tag);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array|array[]
+     */
+    public function createDocumentDataProvider(): array
+    {
+        return [
+            'valid xml, not validate' => [
+                '<?xml version="1.0" encoding="UTF-8"?><tag></tag>',
+                'tag',
+                null,
+                false,
+            ],
+            'invalid xml, not validate' => [
+                '<?xml version="1.0" encoding="UTF-8"?><!<tag></tag>',
+                'tag',
+                null,
+                true,
+            ],
+            'unexpected xml, not validate' => [
+                '<?xml version="1.0" encoding="UTF-8"?><another></another>',
+                'another',
+                null,
+                false,
+            ],
+            'valid xml, validate' => [
+                '<?xml version="1.0" encoding="UTF-8"?><tag></tag>',
+                'tag',
+                __DIR__ . '/DocumentFactoryTest.xsd',
+                false,
+            ],
+            'unexpected xml, validate' => [
+                '<?xml version="1.0" encoding="UTF-8"?><another></another>',
+                'another',
+                __DIR__ . '/DocumentFactoryTest.xsd',
+                true,
+            ],
+        ];
+    }
+}
