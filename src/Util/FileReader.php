e9f7cf8aad12e6e0e7b09d360da1e830fde312b3
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2020-06-23 23:08:47 +0200

    add file reader

---
+++ src/Util/FileReader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace VStelmakh\Covelyzer\Util;
+
+class FileReader
+{
+    /**
+     * @param string $filePath
+     * @return string
+     */
+    public function getContents(string $filePath): string
+    {
+        if (!is_file($filePath)) {
+            throw new \InvalidArgumentException(sprintf('File "%s" not exist', $filePath));
+        }
+
+        if (!is_readable($filePath)) {
+            throw new \RuntimeException(sprintf('File "%s" not readable', $filePath));
+        }
+
+        $contents = @file_get_contents($filePath);
+
+        if ($contents === false) {
+            throw new \RuntimeException(sprintf('File "%s" get contents error', $filePath));
+        }
+
+        return $contents;
+    }
+}
---
+++ tests/Util/FileReaderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace VStelmakh\Covelyzer\Tests\Util;
+
+use VStelmakh\Covelyzer\Util\FileReader;
+use PHPUnit\Framework\TestCase;
+
+class FileReaderTest extends TestCase
+{
+    /**
+     * @dataProvider getContentsDataProvider
+     */
+    public function testGetContents(string $filePath, bool $expectException, string $expected): void
+    {
+        $fileReader = new FileReader();
+        if ($expectException) {
+            $this->expectException(\InvalidArgumentException::class);
+        }
+        $actual = $fileReader->getContents($filePath);
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @return array|array[]
+     */
+    public function getContentsDataProvider(): array
+    {
+        return [
+            [__DIR__ . '/nonexistent_file', true, ''],
+            [__DIR__ . '/FileReaderTest.txt', false, "Test content\n"],
+        ];
+    }
+}
